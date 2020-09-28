be6b8e8e9fa3e76a80363cdab57c43b7e0ecd062
TheSil <sildak@gmail.com>
2020-09-28 12:15:19 +0200

    reporting improved

--- polynomial_factors.py
+++ polynomial_factors.py
@@ -1,11 +1,13 @@
 import argparse
 from copy import deepcopy
-from expressions import Variable, ASSUMED_1, ASSUMED_OPEN_INTERVAL_0_TO_1, ASSUMED_CLOSED_INTERVAL_0_TO_1, \
+from expressions import Variable, Multiplication, ASSUMED_0, ASSUMED_1, ASSUMED_OPEN_INTERVAL_0_TO_1, ASSUMED_CLOSED_INTERVAL_0_TO_1, \
     ASSUMED_0_OR_1
 from rules import basic_rules, check_remaining_coeffs, check_01_coeffs, check_terms
 from contradiction import Contradiction
 from proof import Proof
 import proof
+
+counterexamples = open('counterexamples.log','w')
 
 
 class PolynomialProductAssumptions:
@@ -27,19 +29,114 @@ class PolynomialProductAssumptions:
         # we can safely assume that constant coefficients are 1
         self.assumed_a[0].adjust(ASSUMED_1, 2, init_proof)
         self.assumed_b[0].adjust(ASSUMED_1, 2, init_proof)
+        # also by pencil/paper argument we have following
+        self.assumed_b[self.deg_p].adjust(ASSUMED_0, 2, init_proof)
+        self.assumed_b[self.deg_q - self.deg_p].adjust(ASSUMED_0, 2, init_proof)
         init_proof.print()
         self.additional_assumptions = []
 
     def __str__(self):
-        # print all the non-trivial assumptions
+        # # print all the non-trivial assumptions
+        # res = ""
+        # for i in range(1, self.deg_p):
+        #     if self.assumed_a[i].assumed_type != ASSUMED_CLOSED_INTERVAL_0_TO_1:
+        #         res += f"{self.assumed_a[i]}, "
+        # for i in range(1, self.deg_q):
+        #     if self.assumed_b[i].assumed_type != ASSUMED_CLOSED_INTERVAL_0_TO_1:
+        #         res += f"{self.assumed_b[i]}, "
+        # return res
+
+        # print all the non-trivial assumptions in Mathematica syntax
         res = ""
-        for i in range(1, self.deg_p):
-            if self.assumed_a[i].assumed_type != ASSUMED_CLOSED_INTERVAL_0_TO_1:
-                res += f"{self.assumed_a[i]}, "
-        for i in range(1, self.deg_q):
-            if self.assumed_b[i].assumed_type != ASSUMED_CLOSED_INTERVAL_0_TO_1:
-                res += f"{self.assumed_b[i]}, "
+        res += f"n={self.deg_r};"
+        res += f"degP={self.deg_p};"
+        res += f"degQ={self.deg_q};"
+
+        res += "p={"
+        for i in range(0, self.deg_p+1):
+            if self.assumed_a[i].assumed_type == ASSUMED_0:
+                res += f"0"
+            elif self.assumed_a[i].assumed_type == ASSUMED_1:
+                res += f"1"
+            else:
+                res += f"a[{i}]"
+            if i != self.deg_p:
+                res += ","
+        res += "};"
+
+        res += "po={"
+        first=True
+        for i in range(0, self.deg_p+1):
+            if self.assumed_a[i].assumed_type == ASSUMED_OPEN_INTERVAL_0_TO_1:
+                if not first:
+                    res += ","
+                res += f"a[{i}]"
+                first = False
+        res += "};"
+
+        res += "q={"
+        for i in range(0, self.deg_q+1):
+            if self.assumed_b[i].assumed_type == ASSUMED_0:
+                res += f"0"
+            elif self.assumed_b[i].assumed_type == ASSUMED_1:
+                res += f"1"
+            else:
+                res += f"b[{i}]"
+            if i != self.deg_q:
+                res += ","
+        res += "};"
+
+        res += "qo={"
+        first=True
+        for i in range(0, self.deg_q+1):
+            if self.assumed_b[i].assumed_type == ASSUMED_OPEN_INTERVAL_0_TO_1:
+                if not first:
+                    res += ","
+                res += f"b[{i}]"
+                first = False
+        res += "};"
+
+        res += "r={"
+
+        for k in range(self.deg_r + 1):
+            open_idxs = []
+            closed_idxs = []
+            ones_idxs = []
+            zeroes_idxs = []
+            zero_or_one_idxs = []
+            for i in range(max(0, k - self.deg_q), min(self.deg_p, k) + 1):
+                # a_i * b_j
+                j = k - i
+                ab_assumption = Multiplication(self.assumed_a[i], self.assumed_b[j])
+                if ab_assumption.assumed_type == ASSUMED_0:
+                    zeroes_idxs.append(i)
+                elif ab_assumption.assumed_type == ASSUMED_1:
+                    ones_idxs.append(i)
+                elif ab_assumption.assumed_type == ASSUMED_OPEN_INTERVAL_0_TO_1:
+                    open_idxs.append(i)
+                elif ab_assumption.assumed_type == ASSUMED_CLOSED_INTERVAL_0_TO_1:
+                    closed_idxs.append(i)
+                elif ab_assumption.assumed_type == ASSUMED_0_OR_1:
+                    zero_or_one_idxs.append(i)
+                else:
+                    raise Exception("Unknown assumed type")
+
+            if len(closed_idxs) == 0 and len(open_idxs) == 0 and len(ones_idxs) == 0 and len(zero_or_one_idxs)==0:
+                res += f"0"
+            elif len(open_idxs) > 0 or len(ones_idxs) > 0:
+                res += f"1"
+            else:
+                res += f"c[{k}]"
+            if k != self.deg_r:
+                res += ","
+        res += "};"
+
+
+
         return res
+
+
+
 
 
 def check_factorization(a, b):
@@ -107,7 +204,8 @@ def check_factorization(a, b):
 
             print(f" Failed to find contradiction for n={n},a={a},b={b} when assuming"
                   f" a_{i} in (0,1) is smallest with this property")
-            print("", tmp_assumptions)
+            counterexamples.write(str(tmp_assumptions)+'\n')
+            counterexamples.flush()
             # return False  # comment this to see all fails for given degree
         except Contradiction:
             pass
@@ -128,8 +226,8 @@ def check_factorization(a, b):
 
 def check_degree(deg_r):
     result = True
-    # degree of any counterexample >= 6 by paper/pencil proof
-    for deg_a in range(6, deg_r // 2 + 1):
+    # degree of any counterexample >= 5 by paper/pencil proof
+    for deg_a in range(6, (deg_r - 1) // 2 + 1):
         deg_b = deg_r - deg_a
         if not check_factorization(deg_a, deg_b):
             result = False
